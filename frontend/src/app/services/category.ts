ebcceb37cd6ad97dbcf8002ed3ca02a9f47c7cd3
Aaron Torres <221193@unamba.edu.pe>
2026-01-13 19:27:19 -0500

    feat: add angular services to consume categories and products

---
+++ frontend/src/app/services/category.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CategoryService {
+  private api = 'http://localhost:8080/api/categories';
+  constructor(private http: HttpClient) {}
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.api);
+  }
+}
