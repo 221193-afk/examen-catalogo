7445c0184338a7e7e806ac83a04fddad29a40136
Aaron Torres <221193@unamba.edu.pe>
2026-01-13 19:29:18 -0500

    feat: build bootstrap ui with select and products table with filtering

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './services/category';
+import { ProductService } from './services/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent implements OnInit {
+
+  categories: any[] = [];
+  products: any[] = [];
+
+  selectedCategoryId: number = 0;
+
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryService.getAll().subscribe(data => {
+      this.categories = data;
+
+      if (this.categories.length > 0) {
+        this.selectedCategoryId = this.categories[0].id;
+        this.loadProducts(this.selectedCategoryId);
+      }
+    });
+  }
+
+  onCategoryChange(event: any) {
+    const id = Number(event.target.value);
+    this.selectedCategoryId = id;
+    this.loadProducts(id);
+  }
+
+  loadProducts(id: number) {
+    this.productService.byCategory(id).subscribe(data => {
+      this.products = data;
+    });
+  }
 }
